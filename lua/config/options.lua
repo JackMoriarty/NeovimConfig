15218321b1fd1687806fe896b442e081c10f07f1
chenbangduo <chenbangduo@foxmail.com>
2026-01-22 19:47:45 +0800

    update font size

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 if vim.g.neovide then
-  vim.opt.guifont = "Maple Mono NF CN:14"
+  vim.opt.guifont = "Maple Mono NF CN:h12"
 end
