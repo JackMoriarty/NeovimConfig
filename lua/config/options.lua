73e824cf2593a341d8150aedbab270e5bdcf0b7c
JackMoriarty <chenbangduo@qq.com>
2026-02-11 20:59:04 +0800

    add 80 col ruler.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- neovide 字体设置
 if vim.g.neovide then
   vim.opt.guifont = "Maple Mono NF CN:h12"
 end
+
+-- 显示80列辅助线
+vim.opt.colorcolumn = "80"
