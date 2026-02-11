75186a96f27435c87ec561ef065481821959c0c8
JackMoriarty <chenbangduo@qq.com>
2026-02-11 21:00:45 +0800

    replace solarized theme with gruvbox theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,47 @@
+-- return {
+--   "maxmx03/solarized.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   ---@type solarized.config
+--   opts = {},
+--   config = function(_, opts)
+--     vim.o.termguicolors = true
+--     vim.o.background = "light"
+--     require("solarized").setup(opts)
+--     vim.cmd.colorscheme("solarized")
+--   end,
+-- }
+
 return {
-  "maxmx03/solarized.nvim",
-  lazy = false,
+  "ellisonleao/gruvbox.nvim",
   priority = 1000,
-  ---@type solarized.config
-  opts = {},
+  opts = {
+    terminal_colors = true, -- add neovim terminal colors
+    undercurl = true,
+    underline = true,
+    bold = true,
+    italic = {
+      strings = true,
+      emphasis = true,
+      comments = true,
+      operators = false,
+      folds = true,
+    },
+    strikethrough = true,
+    invert_selection = false,
+    invert_signs = false,
+    invert_tabline = false,
+    inverse = true, -- invert background for search, diffs, statuslines and errors
+    contrast = "", -- can be "hard", "soft" or empty string
+    palette_overrides = {},
+    overrides = {},
+    dim_inactive = false,
+    transparent_mode = false,
+  },
   config = function(_, opts)
     vim.o.termguicolors = true
-    vim.o.background = "light"
-    require("solarized").setup(opts)
-    vim.cmd.colorscheme("solarized")
+    vim.o.background = "dark"
+    require("gruvbox").setup(opts)
+    vim.cmd.colorscheme("gruvbox")
   end,
 }
