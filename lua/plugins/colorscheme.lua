98c775cea9470ddf7f8e91c379a5c9d39de48eac
JackMoriarty <chenbangduo@qq.com>
2026-02-16 22:41:06 +0800

    support auto-dark.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,3 @@
--- return {
---   "maxmx03/solarized.nvim",
---   lazy = false,
---   priority = 1000,
---   ---@type solarized.config
---   opts = {},
---   config = function(_, opts)
---     vim.o.termguicolors = true
---     vim.o.background = "light"
---     require("solarized").setup(opts)
---     vim.cmd.colorscheme("solarized")
---   end,
--- }
-
 return {
   "ellisonleao/gruvbox.nvim",
   priority = 1000,
@@ -40,7 +26,6 @@ return {
   },
   config = function(_, opts)
     vim.o.termguicolors = true
-    vim.o.background = "dark"
     require("gruvbox").setup(opts)
     vim.cmd.colorscheme("gruvbox")
   end,
