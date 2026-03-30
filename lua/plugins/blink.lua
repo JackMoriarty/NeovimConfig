06d3ffdc78909d2336515340b3b25dc95ad7a5aa
JackMoriarty <chenbangduo@qq.com>
2026-03-30 21:32:54 +0800

    update config of minuet.

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,7 +1,10 @@
 return {
   "saghen/blink.cmp",
-  -- optional: provides snippets for the snippet source
-  dependencies = { "rafamadriz/friendly-snippets" },
+  dependencies = {
+    -- optional: provides snippets for the snippet source
+    "rafamadriz/friendly-snippets",
+    "milanglacier/minuet-ai.nvim",
+  },
 
   -- use a release tag to download pre-built binaries
   version = "1.*",
@@ -56,7 +59,18 @@ return {
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
+      default = { "lsp", "path", "snippets", "buffer", "minuet" },
+      providers = {
+        minuet = {
+          name = "minuet",
+          module = "minuet.blink",
+          async = true,
+          -- Should match minuet.config.request_timeout * 1000,
+          -- since minuet.config.request_timeout is in seconds
+          timeout_ms = 3000,
+          score_offset = 50, -- Gives minuet higher priority among suggestions
+        },
+      },
     },
 
     -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
--- lua/plugins/minuet-ai.lua
+++ lua/plugins/minuet-ai.lua
@@ -1,59 +1,40 @@
 return {
-  {
-    "milanglacier/minuet-ai.nvim",
-    config = function()
-      require("minuet").setup({
-        virtualtext = {
-          auto_trigger_ft = { "*" },
-          disabled_auto_trigger_ft = { "markdown", "text" },
-          keymap = {
-            -- accept whole completion
-            accept = "<A-A>",
-            -- accept one line
-            accept_line = "<A-a>",
-            -- accept n lines (prompts for number)
-            -- e.g. "A-z 2 CR" will accept 2 lines
-            accept_n_lines = "<A-z>",
-            -- Cycle to prev completion item, or manually invoke completion
-            prev = "<A-[>",
-            -- Cycle to next completion item, or manually invoke completion
-            next = "<A-]>",
-            dismiss = "<A-e>",
-          },
+  "milanglacier/minuet-ai.nvim",
+  config = function()
+    require("minuet").setup({
+      provider = "openai_fim_compatible",
+      provider_options = {
+        openai_fim_compatible = {
+          name = "RemoteService",
+          end_point = "[RemoteURL]/completions",
+          api_key = function()
+            return "TERM"
+          end,
+          model = "FIMModel",
         },
-
-        provider = "openai_fim_compatible",
-        provider_options = {
-          openai_fim_compatible = {
-            name = "RemoteService",
-            end_point = "[RemoteURL]/completions",
-            api_key = function()
-              return "TERM"
-            end,
-            model = "FIMModel",
+      },
+    })
+    require("lualine").setup({
+      sections = {
+        lualine_x = {
+          {
+            require("minuet.lualine"),
+            -- the follwing is the default configuration
+            -- the name displayed in the lualine. Set to "provider", "model" or "both"
+            -- display_name = 'both',
+            -- separator between provider and model name for option "both"
+            -- provider_model_separator = ':',
+            -- whether show display_name when no completion requests are active
+            -- display_on_idle = false,
           },
+          "encoding",
+          "fileformat",
+          "filetype",
         },
-      })
-      require("lualine").setup({
-        sections = {
-          lualine_x = {
-            {
-              require("minuet.lualine"),
-              -- the follwing is the default configuration
-              -- the name displayed in the lualine. Set to "provider", "model" or "both"
-              -- display_name = 'both',
-              -- separator between provider and model name for option "both"
-              -- provider_model_separator = ':',
-              -- whether show display_name when no completion requests are active
-              -- display_on_idle = false,
-            },
-            "encoding",
-            "fileformat",
-            "filetype",
-          },
-        },
-      })
-    end,
+      },
+    })
+  end,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
   },
-  { "nvim-lua/plenary.nvim" },
 }
